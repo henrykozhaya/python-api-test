975dfe51ffd0ead66f9b2887c48a4bb6f1bd16e6
Henry Kozhaya <henry.kozhaya@gmail.com>
2022-01-29 20:39:17 +0200

    Added print for troubleshooting

--- API.py
+++ API.py
@@ -1,12 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def show_all():
+    print("Hello")
     return "1"
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=80)
