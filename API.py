423465c272b087be69e31767c22fe97a2e655fe3
Henry Kozhaya <henry.kozhaya@gmail.com>
2022-02-07 19:10:04 +0200

    4th commit

--- API.py
+++ API.py
@@ -10,5 +10,5 @@ def show_all():
 
 
 if __name__ == '__main__':
-    # app.run(debug=False, host='0.0.0.0', port=8080)
-    app.run(debug=False)
+    app.run(debug=False, host='127.0.0.1', port=8000)
+    # app.run(debug=False)
