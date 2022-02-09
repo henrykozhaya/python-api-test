183d72dfd247c060d5e87b7492435714a02ab219
Henry Kozhaya <henry.kozhaya@gmail.com>
2022-02-09 13:13:38 +0200

    Version 1.0.0: Logs added

--- OK.py
+++ OK.py
@@ -1,1 +1,10 @@
-print("OK")
+import datetime
+import json
+
+def log_to_file(obj):
+    open('logs.txt', 'a').write(
+        datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + " | " + json.dumps(obj) + '\n'
+    )
+obj = {"message":"OK"}
+log_to_file(obj)
+print (obj)
